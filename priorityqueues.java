8e50430a2f6d968b119c0e6bbbc0fd276d9616af
gunashree9588 <gunashree9581@gmail.com>
2026-08-18 22:47:22 +0530

    Add files via upload

--- priorityqueues.java
+++ priorityqueues.java
@@ -1,22 +1,22 @@
 import java.util.*;
 public class priorityqueues {
     public static void main(String[] args){
-        PriorityQueue<Double> pq = new PriorityQueue<Double>(Collections.reverseOrder());     // collections means elements in the pq
-        pq.offer(3.5);
-        pq.offer(5.6);
-        pq.offer(7.8);
-        pq.offer(8.8);
-        while(!pq.isEmpty()){     // pq is non empty means 
-            System.out.println(pq.poll());
-            // output
-            //8.8
-            //7.8
-            //5.6
-            //3.5
-
-
+        PriorityQueue<String> que = new PriorityQueue<String>();
+        que.offer("D");
+        que.offer("B");
+        que.offer("C");
+        que.offer("A");
+        while(!que.isEmpty()){
+            System.out.println(que.poll());
+             //output
+             //A
+             //B
+             //C
+             //D
         }
 
+
+     
     }
 
 }
